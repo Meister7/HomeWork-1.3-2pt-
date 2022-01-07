d27dc4d71f281ae6aad1590fff78ee4a3b631c95
Meister7 <emirkarmyshev7@gmail.com>
2022-01-07 21:07:21 +0600

    Initial Commit

---
+++ HomeWork#1.3(2pt)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork#1.3(2pt)
+//
+//  Created by Эмир Кармышев on 7/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
